ff2d483aa165406d3c23236f8384c16e5ab123e5
steadfastie <lkumbrella@outlook.com>
2025-01-18 15:47:55 +0000

    first handler

---
+++ dotnet/Services/OrderService.cs
@@ -0,0 +1,41 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Liaison.Proto;
+using Status = Liaison.Proto.Status;
+using static Liaison.Proto.OrderService;
+
+namespace dotnet.Services;
+
+public class OrderService : OrderServiceBase
+{
+    public override Task<Response> CreateOrder(Request request, ServerCallContext context)
+    {
+        var resp = new Response
+        {
+            OrderId = Guid.NewGuid().ToString(),
+            Status = Status.Valid,
+            Message = "test",
+            ReceivedAt = Timestamp.FromDateTime(DateTime.UtcNow),
+            Duration = Duration.FromTimeSpan(TimeSpan.FromMilliseconds(30))
+        };
+        resp.StatesHistory.AddRange([
+            new State
+            {
+                Timestamp = Timestamp.FromDateTime(DateTime.UtcNow),
+                Status = Status.Received
+            },
+            new State
+            {
+                Timestamp = Timestamp.FromDateTime(DateTime.UtcNow.AddMilliseconds(10)),
+                Status = Status.Received
+            },
+            new State
+            {
+                Timestamp = Timestamp.FromDateTime(DateTime.UtcNow.AddMilliseconds(20)),
+                Status = Status.Received
+            },
+        ]);
+
+        return Task.FromResult(resp);
+    }
+}
